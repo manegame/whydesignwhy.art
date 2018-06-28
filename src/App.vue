afe83f8c3a01f1987c94a21b19ca164de8da9ef3
manegame <manusnijhoff@gmail.com>
2018-06-28 02:40:20 +0200

    added support for one video

--- src/App.vue
+++ src/App.vue
@@ -38,19 +38,21 @@ export default {
   }
 }
 
-// .flip-enter-active,
-// .flip-leave-active {
-//   transition: transform 1s;
-// }
+.embed-video {
+  position: relative; 
+  padding-bottom: 56.25%; 
+  height: 0;
+  overflow: hidden; 
+  max-width: 100%;
 
-// .flip-enter-to,
-// .flip-leave {
-  //   }
-
-// .flip-leave-to,
-// .flip-enter {
-//   transform: translate(0) rotate(0deg);
-//   transform: translate(-45vw) rotate(45deg);
-// }
-
+  iframe, 
+  object, 
+  embed { 
+    position: absolute; 
+    top: 0; 
+    left: 0; 
+    width: 100%;
+    height: 100%;
+  }
+}
 </style>
--- src/components/views/videopage.vue
+++ src/components/views/videopage.vue
@@ -1,17 +1,144 @@
 <template>
-  <div class='bookpage'>
-    Bookpage
+  <div class='videopage'>
+    <div  class='videopage__curtains'
+          @click='openCurtains = !openCurtains'>
+      <div  class='videopage__curtains__left'
+            :class='{ "videopage__curtains__left--open": openCurtains }'></div>
+      <div  class='videopage__curtains__right'
+            :class='{ "videopage__curtains__right--open": openCurtains }'></div>
+    </div>
+    <div class='videopage__video embed-video'>
+      <youtube  :video-id="videoId" 
+                ref="youtube" 
+                :player-vars="playerVars" 
+                @playing="playing" 
+                @ended='ended' />
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'bookpage'
+  name: 'videopage',
+  data () {
+    return {
+      videoId: 'SqUqVBsbaRk',
+      openCurtains: false,
+      playerVars: {
+        autoplay: 0,
+        rel: 0
+      }
+    }
+  },
+  computed: {
+    player () {
+      return this.$refs.youtube.player
+    }
+  },
+  methods: {
+    async playVideo () {
+      await this.player.playVideo()
+      // Do something after the playVideo command
+      console.log('playVideo triggered')
+    },
+    playing () {
+      console.log('we watching!')
+    },
+    ended () {
+      console.log('video ended')
+    }
+  },
+  watch: {
+    openCurtains (val) {
+      if (val) {
+        console.log('curtains open!')
+        this.player.playVideo()
+      } else {
+        this.player.pauseVideo()
+      }
+    }
+  }
 }
 </script>
 
-<style>
-.bookpage {
-  color: yellow;
+<style scoped lang='scss'>
+@import '../../assets/style/variables';
+
+
+.videopage {
+  width: 100vw;
+  height: 100vh;
+  background: black;
+  color: white;
+  overflow: hidden;
+
+  &__curtains {
+    z-index: 5;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    cursor: pointer;
+
+    &__left {
+      position: absolute;
+      left: 0;
+      width: 50%;
+      height: 100%;
+      background: white;
+      background-image: url('../../../static/left.svg');
+      background-position: right;
+      background-repeat: no-repeat;
+      transition: all 0.3s ease-out;
+      transform: none;
+      animation: peek 10s infinite;
+
+      &--open {
+        left: -65vw;
+        transform: skew(-10deg, 0);
+      }
+    }
+
+    &__right {
+      position: absolute;
+      right: 0;
+      width: 50%;
+      height: 100%;
+      background: white;
+      background-image: url('../../../static/right.svg');
+      background-position: left;
+      background-repeat: no-repeat;
+      transition: all 0.3s ease-out;
+
+      &--open {
+        right: -65vw;
+        transform: skew(10deg, 0);
+      }
+    }
+  }
+
+  &__video {
+    z-index: 4;
+    width: 60vw;
+    margin-left: 20vw;
+  }
+}
+
+@keyframes peek {  
+  0% {
+    transform: initial;
+  }
+  25% {
+    transform: initial;
+  }
+  30% {
+    transform: skew(-4deg, 0);
+  }
+  35% {
+    transform: initial;
+  }
+  100% {
+    transform: initial;
+  }
 }
 </style>
